30cdfc4614bf9ae8df9e51ce0a05b7d08a083cc5
Villaflor, Justin John Mico <justinjohnmico.09@gmail.com>
2024-10-11 01:37:13 +0800

    Update asgi.py

--- modelsapi/asgi.py
+++ modelsapi/asgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter # type: ignore
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modelsapi.settings')
